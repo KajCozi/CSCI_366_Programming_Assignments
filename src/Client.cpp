5119aebf618076d76a01d8fa01ba34683d85a741
kajcozi <kajiacoziahr@gmail.com>
2020-04-18 14:01:06 -0600

    Put in code from PA1

--- src/Client.cpp
+++ src/Client.cpp
@@ -22,23 +22,155 @@ Client::~Client() {
 
 
 void Client::initialize(unsigned int player, unsigned int board_size){
+    //Make sure to distinguish between parameter and actual player/board_size from Client
+
+    Client::player = player;
+    Client::board_size = board_size;
+
+//create a 2D vector
+//Used geeksforgeeks : https://www.geeksforgeeks.org/2d-vector-in-cpp-with-user-defined-size/
+
+    vector<vector<int>> board;
+    board.resize(board_size, std::vector<int>(board_size, 0));
+
+//Convert numeric value to string to allow it to be added to the file name
+    string playerNum = std::to_string(player);
+
+//Used github example: https://github.com/USCiLab/cereal/tree/a5a30953125e70b115a28dd76b64adf3c97cc883
+    std::ofstream create("player_" + playerNum + ".action_board.json");
+    cereal::JSONOutputArchive archive_out(create);
+    archive_out(CEREAL_NVP(board));
+
+//Sets the player number and player action board for client according to id
+    if(player == 1){
+        board_name = "player_1.action_board.json";
+    }
+
+    if(player = 2){
+        board_name = "player_2.action_board.json";
+    }
 }
 
 
 void Client::fire(unsigned int x, unsigned int y) {
+    //Write shot to file
+    if(player == 1){
+        std::ofstream FireShot1("player_1.shot.json");
+        cereal::JSONOutputArchive shot1(FireShot1);
+        shot1(CEREAL_NVP(x), CEREAL_NVP(y));
+    }
+
+    if(player == 2){
+        std::ofstream FireShot2("player_2.shot.json");
+        cereal::JSONOutputArchive shot2(FireShot2);
+        shot2(CEREAL_NVP(x), CEREAL_NVP(y));
+    }
 }
 
 
 bool Client::result_available() {
+
+    //Check to see if result file is available
+    if(player == 1){
+
+        ifstream fileAvailable1("player_1.result.json");
+
+        if(fileAvailable1.good()) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    else if(player == 2){
+
+        ifstream fileAvailable2("player_2.result.json");
+
+        if(fileAvailable2.good()) {
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
 
 int Client::get_result() {
+    int p1ReturnNum;
+    int p2ReturnNum;
+
+    if(player == 1){
+
+        ifstream result1("player_1.result.json");
+        cereal::JSONInputArchive p1Result(result1);
+        p1Result(p1ReturnNum);
+
+        remove("player_1.result.json");
+
+        if(p1ReturnNum == 1){
+            return HIT;
+        }
+        if(p1ReturnNum == -1){
+            return MISS;
+        }
+        if(p1ReturnNum == 0){
+            return OUT_OF_BOUNDS;
+        }else{
+            throw ClientException("Error");
+        }
+    }
+
+    if(player == 2){
+        ifstream result2("player_2.result.json");
+        cereal::JSONInputArchive p2Result(result2);
+        p2Result(p2ReturnNum);
+
+        remove("player_2.result.json");
+
+
+        if(p2ReturnNum == 1){
+            return HIT;
+        }
+        if(p2ReturnNum == -1){
+            return MISS;
+        }
+        if(p2ReturnNum == 0){
+            return OUT_OF_BOUNDS;
+        }else{
+            throw ClientException("Error");
+        }
+    }
 }
 
 
 
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+    vector<vector<int>> action;
+
+//Used: https://uscilab.github.io/cereal/assets/doxygen/group__Utility.html
+
+    if(player == 1){
+
+        ifstream board_in("player_1.action_board.json");
+        cereal::JSONInputArchive update_in(board_in);
+        update_in(action);
+        action[x][y] = result;
+
+        ofstream board_out("player_1.action_board.json");
+        cereal::JSONOutputArchive update_out(board_out);
+        update_out(cereal::make_nvp("board", action));
+
+    }else if(player == 2){
+
+        ifstream board_in("player_2.action_board.json");
+        cereal::JSONInputArchive update_in(board_in);
+        update_in(action);
+        action[x][y] = result;
+
+        ofstream board_out("player_2.action_board.json");
+        cereal::JSONOutputArchive update_out(board_out);
+        update_out(cereal::make_nvp("board", action));
+
+    }
 }
 
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -25,6 +25,10 @@
  * @param file - the file whose length we want to query
  * @return length of the file in bytes
  */
+
+char arrayboard1 [BOARD_SIZE][BOARD_SIZE];
+char arrayboard2 [BOARD_SIZE][BOARD_SIZE];
+
 int get_file_length(ifstream *file){
 }
 
@@ -32,6 +36,46 @@ int get_file_length(ifstream *file){
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+    //Figured out that I was originally comparing BOARD_SIZE to the parameter instead of the actual board_size of players 1 and 2
+    this->board_size = board_size;
+
+//Determines if board size is wrong
+    if (this->board_size != BOARD_SIZE){
+        throw ServerException("The board size is wrong");
+    }
+
+//Determines if file name is bad
+    if(p1_setup_board !=  "player_1.setup_board.txt" && p2_setup_board != "player_2.setup_board.txt"){
+        throw ServerException("Bad file name");
+    }
+
+
+//Used this site: http://www.cplusplus.com/doc/tutorial/files/
+//read in the file for player 1
+
+    ifstream begin1("player_1.setup_board.txt");
+
+    for(int i=0; i < BOARD_SIZE; i++){
+        for(int j=0; j < BOARD_SIZE; j++){
+            begin1 >> arrayboard1[i][j];
+//                 cout << arrayboard1[i][j];
+        }
+//            cout << "\n";
+    }
+
+//read in the file for player
+    ifstream begin2("player_2.setup_board.txt");
+
+    for(int i=0; i < BOARD_SIZE; i++) {
+//            cout << i;
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            begin2 >> arrayboard2[i][j];
+//                cout << arrayboard2[i][j];
+//                cout << j;
+        }
+//            cout << "\n";
+    }
+
 }
 
 
@@ -43,9 +87,98 @@ BitArray2D *Server::scan_setup_board(string setup_board_name){
 }
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+    //Determines if player number is too low or too high
+    if (player < 1 || player > 2) {
+        throw ServerException("Player number out of bounds");
+    }
+
+//Determines if shot was out of bounds for X and Y
+    if (x >= board_size || y >= board_size) {
+        return OUT_OF_BOUNDS;
+    }
+
+//Determines if hit or miss was detected
+
+//Swapped order of x & y because incorrect output was being produced
+    if(player == 1){
+        if(arrayboard2[y][x] == 'C' || arrayboard2[y][x] == 'B' || arrayboard2[y][x] == 'R'||arrayboard2[y][x] == 'S'||arrayboard2[y][x] == 'D'){
+            return HIT;
+        }
+        else{
+            return MISS;
+        }
+    }
+
+    if(player == 2){
+        if(arrayboard1[y][x] == 'C' || arrayboard1[y][x] == 'B' || arrayboard1[y][x] == 'R'||arrayboard1[y][x] == 'S'||arrayboard1[y][x] == 'D'){
+            return HIT;
+        }
+        else{
+            return MISS;
+        }
+    }
+
 }
 
 
 int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
+    int x;
+    int y;
+
+//Determines if player number is too low or too high
+    if (player < 1 || player > 2) {
+        throw ServerException("Wrong number of players");
+    }
+
+//Used Stack Overflow: https://stackoverflow.com/questions/32205981/reading-json-files-in-c?fbclid=IwAR2mE88zb0yzC55CeEAdr-szLUxcQFF9cwGoJ5sJ8Sn-Tr0V_vB2C-sgvy8
+//read 1
+    std::ifstream shot_file_read1("player_1.shot.json");
+
+    if(shot_file_read1.fail())
+        return NO_SHOT_FILE;
+
+//Used Mike Wittie's example on github
+    cereal::JSONInputArchive archive_in1(shot_file_read1);
+    archive_in1(x,y);
+
+    /*Help from Cooper - result is found elsewhere in the files, tried
+     *  use "outcome" as my variable name. However this did not
+     *  work so we changed to result*/
+
+    int result;
+    result = evaluate_shot(player, x, y);
+    printf("\n\n");
+    printf("%d", result);
+    printf("\n\n");
+
+//write 1
+    std::ofstream shot_file_write1("player_1.result.json");
+    cereal::JSONOutputArchive archive_out1(shot_file_write1);
+    archive_out1(CEREAL_NVP(result));
+
+    remove("player_1.shot.json");
+
+
+//read 2
+    std::ifstream shot_file_read2("player_1.shot.json");
+
+    if(shot_file_read2.fail())
+        return NO_SHOT_FILE;
+
+    cereal::JSONInputArchive archive_in2(shot_file_read2);
+    archive_in2(x,y);
+
+    result = evaluate_shot(player, x, y);
+    printf("\n\n");
+    printf("%d", result);
+    printf("\n\n");
+
+//write 2
+    std::ofstream shot_file_write("player_1.result.json");
+    cereal::JSONOutputArchive archive_out2(shot_file_write);
+    archive_out2(CEREAL_NVP(result));
+
+    remove("player_1.shot.json");
+
+    return SHOT_FILE_PROCESSED;
 }
